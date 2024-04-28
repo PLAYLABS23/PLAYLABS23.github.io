0b00b8e018a7b2300f057d660050ced7c9d2392c
강찬영 <backgwa@icloud.com>
2024-04-29 03:02:50 +0900

    마그네틱 스크롤 방식 변경

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 // page init
 function init() {
     video = document.getElementById('bg-video');
-    sections = document.querySelectorAll('section');
 }
 
 // page main script
@@ -10,19 +9,4 @@ function main() {
     video.playbackRate = 0.75;
     video.playsInline = true;
     video.play();
-
-    // scroll section event
-    observer = new IntersectionObserver((e) => {
-        e.forEach((section) => {
-            if (section.isIntersecting) {
-                const postion = section.target.offsetTop;
-                window.scrollTo({ top: postion, behavior: 'smooth' });
-            }
-        })
-    }, { threshold: 0.01 });
-
-    // scroll section checking
-    sections.forEach((section) => {
-        observer.observe(section);
-    });
 }
